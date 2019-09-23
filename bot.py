adbafd787b521edbb3a291093f2d610af678766b
ar0n <asimpole.as@gmail.com>
2019-09-23 02:38:21 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
 import json
 import asyncio
 
-bot = commands.Bot(command_prefix=['$'])
+bot = commands.Bot(command_prefix='$')
 
 key = "BOT_KEY_HERE"
 
@@ -13,34 +12,30 @@ async def on_ready():
     print (bot.user.name + ' is ready')
 
 @bot.command(pass_context=True)
-@commands.has_permissions(administrator=True)
 async def vanitygen(ctx, *, vanity):
-    """Only ??? can run.
-
-    """
-    if ctx.message.author.id == 'YOUR_DISCORD_ID_HERE':
-        global crunching
-        crunching = 1
-        attempt = 0
-        alert = await bot.say('Discord Invite VanityGen has begun.')
-        while crunching == 1:
-            inv = await bot.create_invite(ctx.message.channel)
-            invend = str(inv)[18:]
-            if vanity.lower() in invend.lower():
-                await bot.say(inv)
-                crunching = 0
-            else:
-                attempt += 1
-                alertNew = 'Discord Invite VanityGen has begun. :x:' + str(attempt)
-                await bot.edit_message(alert, new_content=alertNew)
-                await bot.delete_invite(inv)
-            await asyncio.sleep(1)
+    global crunching
+    crunching = 1
+    attempt = 0
+    alert = await ctx.send('Discord Invite VanityGen has begun.')
+    general = bot.get_channel(CHANNEL_ID_HERE)
+    while crunching == 1:
+        inv = await general.create_invite()
+        invend = str(inv)[18:]
+        if vanity.lower() in invend.lower():
+            await ctx.send(inv)
+            crunching = 0
+        else:
+            attempt += 1
+            alertNew = 'Discord Invite VanityGen has begun. :x:' + str(attempt)
+            await alert.edit(content=alertNew)
+            await inv.delete()
+        await asyncio.sleep(1)
 
 @bot.command(pass_context=True)
-@commands.has_permissions(administrator=True)
 async def stop(ctx):
     global crunching
     crunching = 0
-    await bot.say('VanityGen halted.')
+    await ctx.send('VanityGen halted.')
+
 
 bot.run(key)
